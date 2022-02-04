c36cf820739e8f48113e2fdd2f22d3e24ea72ddd
Jack <jack@example.com>
2022-02-04 10:58:44 +0700

    Finish Step 2

--- app/src/main/java/com/example/listmaker/MainActivity.kt
+++ app/src/main/java/com/example/listmaker/MainActivity.kt
@@ -2,17 +2,46 @@ package com.example.listmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.example.listmaker.databinding.MainActivityBinding
 import com.example.listmaker.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: MainActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+
+        binding.taskListAddButton.setOnClickListener {
+            showCreateListDialog()
+        }
+    }
+
+    private fun showCreateListDialog() {
+        val dialogTitle = getString(R.string.name_of_list)
+        val positiveButtonTitle = getString(R.string.create_list)
+
+        val builder = AlertDialog.Builder(this)
+        val listTitleEditText = EditText(this)
+        listTitleEditText.inputType = InputType.TYPE_CLASS_TEXT
+
+        builder.setTitle(dialogTitle)
+        builder.setView(listTitleEditText)
+
+        builder.setPositiveButton(positiveButtonTitle) { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        builder.create().show()
     }
 }
